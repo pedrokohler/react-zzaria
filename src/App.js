52807689c8a6ea7da667e20e5f2abc80c8a6026b
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-04-26 16:51:42 -0300

    Add material-ui and update index.html to portuguese.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { CssBaseline } from '@material-ui/core';
 
 import MainPage from 'pages/main';
 import LoginPage from 'pages/login';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/login' component={LoginPage} />
-      <Route component={MainPage} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <CssBaseline />
+
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' component={LoginPage} />
+        <Route component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
