f1fc03f31f9c0fa38a0fdbae701b350d194db201
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-04-29 08:32:02 -0300

    Add redirect to flavours page

--- src/containers/choose-pizza-flavour/choose-pizza-flavour.js
+++ src/containers/choose-pizza-flavour/choose-pizza-flavour.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
+import { HOME_PAGE } from 'routes';
 import Title from 'components/title';
 import Header from 'components/content-header';
 import singularOrPlural from 'utils/singularOrPlural';
 
 const ChoosePizzaFlavour = ({ location }) => {
+  if (!location.state) {
+    return <Redirect to={HOME_PAGE} />;
+  }
+
   const { flavours } = location.state;
   return (
     <>
