cb4091bc7326afcfa771c333fc6d5f4909900a0a
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-04-29 19:35:22 -0300

    Add pizzas grid to choose-pizza-flavours

--- src/containers/choose-pizza-flavour/choose-pizza-flavour.js
+++ src/containers/choose-pizza-flavour/choose-pizza-flavour.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import {
+  Card,
+  Grid,
+  Typography
+} from '@material-ui/core';
 
 import { HOME_PAGE } from 'routes';
 import Title from 'components/title';
 import Header from 'components/content-header';
+import PizzasGrid from 'components/pizzas-grid';
+import Divider from 'components/divider';
 import singularOrPlural from 'utils/singularOrPlural';
+
+import pizzaFlavours from 'mocks/pizza-flavours';
 
 const ChoosePizzaFlavour = ({ location }) => {
   if (!location.state) {
@@ -13,6 +23,7 @@ const ChoosePizzaFlavour = ({ location }) => {
   }
 
   const { flavours } = location.state;
+  const { id: sizeId } = location.state;
   return (
     <>
       <Header>
@@ -20,6 +31,18 @@ const ChoosePizzaFlavour = ({ location }) => {
           Escolha até {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')}
         </Title>
       </Header>
+      <PizzasGrid>
+        {pizzaFlavours.map(flavour => (
+          <Grid item key={flavour.id} xs>
+            <Card>
+              <Img src={flavour.image} alt={flavour.name} />
+              <Divider />
+              <Typography>{flavour.name}</Typography>
+              <Typography variant='h5'>{flavour.value[sizeId]}</Typography>
+            </Card>
+          </Grid>
+        ))}
+      </PizzasGrid>
     </>
   );
 };
@@ -27,5 +50,9 @@ const ChoosePizzaFlavour = ({ location }) => {
 ChoosePizzaFlavour.propTypes = {
   location: PropTypes.object.isRequired
 };
+
+const Img = styled.img`
+max-width: 200px;
+`;
 
 export default ChoosePizzaFlavour;
