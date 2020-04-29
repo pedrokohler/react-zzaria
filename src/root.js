f47e840575c736507d92a1b68610624a4fac41a5
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-04-29 20:33:01 -0300

    Add theme to root

--- src/root.js
+++ src/root.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from 'styled-components';
+import {
+  CssBaseline,
+  createMuiTheme,
+  MuiThemeProvider
+} from '@material-ui/core';
 
 import AuthProvider from 'contexts/auth';
 
 import App from 'app';
 
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
+
 const Root = () => {
   return (
     <>
-      <AuthProvider>
-        <CssBaseline />
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <CssBaseline />
 
-        <BrowserRouter>
-          <Route component={App} />
-        </BrowserRouter>
-      </AuthProvider>
+            <BrowserRouter>
+              <Route component={App} />
+            </BrowserRouter>
+          </AuthProvider>
+        </ThemeProvider>
+      </MuiThemeProvider>
     </>
   );
 };
