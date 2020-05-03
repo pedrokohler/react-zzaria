0f5298f5a38a7d8666c3d48ffd19cd65180f2770
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-05-03 15:57:18 -0300

    Add order summary to checkout page

--- src/containers/checkout/checkout.js
+++ src/containers/checkout/checkout.js
@@ -9,53 +9,66 @@ import {
   TextField as MaterialTextField,
   Typography
 } from '@material-ui/core';
+
+import { useOrder } from 'hooks';
 import MyTitle from 'components/title';
+import { singularOrPlural } from 'utils';
 
-const Checkout = () => (
-  <Grid container spacing={4}>
-    <Grid item xs={12} md={6}>
-      <Title>Qual o endereço para entrega?</Title>
-      <PaperContainer>
-        <Grid container spacing={1}>
-          <TextField label='CEP' sm={5} autoFocus />
-          <Grid item sm={7} />
-          <TextField label='Rua' sm={9} />
-          <TextField label='Número' sm={3} />
-          <TextField label='Complemento' sm={12} />
-          <TextField label='Cidade' sm={9} />
-          <TextField label='Estado' sm={3} />
-        </Grid>
-      </PaperContainer>
+const Checkout = () => {
+  const { order } = useOrder();
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12} md={6}>
+        <Title>Qual o endereço para entrega?</Title>
+        <PaperContainer>
+          <Grid container spacing={1}>
+            <TextField label='CEP' sm={5} autoFocus />
+            <Grid item sm={7} />
+            <TextField label='Rua' sm={9} />
+            <TextField label='Número' sm={3} />
+            <TextField label='Complemento' sm={12} />
+            <TextField label='Cidade' sm={9} />
+            <TextField label='Estado' sm={3} />
+          </Grid>
+        </PaperContainer>
 
-      <Title>Qual o seu telefone?</Title>
-      <PaperContainer>
-        <TextField label='Telefone' sm={5} />
-      </PaperContainer>
+        <Title>Qual o seu telefone?</Title>
+        <PaperContainer>
+          <TextField label='Telefone' sm={5} />
+        </PaperContainer>
+      </Grid>
+
+      <Grid container direction='column' item xs={12} md={6}>
+        <Title>Informações do seu pedido</Title>
+        <PaperContainer>
+          <List>
+            {order.pizzas.map((pizza, i) => {
+              const { size, flavours: selectedFlavours, quantity } = pizza;
+              const { name, slices, flavours } = size;
+              return (
+                <ListItem key={i}>
+                  <Typography>
+                    {quantity} - {singularOrPlural(quantity, 'pizza', 'pizzas')}{' '}
+                    <b>{name.toUpperCase()}</b> - {' '}
+                    ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
+                    {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
+                    <br />
+                    {singularOrPlural(selectedFlavours.length, 'No sabor', 'Nos sabores')}{' '}
+                    <b>{selectedFlavours
+                      .map(({ name }) => name)
+                      .join(', ')
+                      .replace(/, (.[^,]*)$/, ' e $1')}
+                    </b>
+                  </Typography>
+                </ListItem>
+              );
+            })}
+          </List>
+        </PaperContainer>
+      </Grid>
     </Grid>
-
-    <Grid container direction='column' item xs={12} md={6}>
-      <Title>Informações do seu pedido</Title>
-      <PaperContainer>
-        <List>
-          <ListItem>
-            <Typography>
-              <b>1</b> pizza <b>MÉDIA</b> (6 fatias, 2 sabores)
-              <br />
-              nos sabores <b>Calabresa e Frango com Catupiry</b>
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <Typography>
-              <b>1</b> pizza <b>MÉDIA</b> (6 fatias, 2 sabores)
-              <br />
-              nos sabores <b>Calabresa e Frango com Catupiry</b>
-            </Typography>
-          </ListItem>
-        </List>
-      </PaperContainer>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 const TextField = ({ sm, xs, autoFocus, ...props }) => (
   <Grid item xs={12} sm={sm}>
