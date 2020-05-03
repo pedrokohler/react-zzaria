f968248bf65927b2b7d37774568e1eea9838b271
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-05-03 18:13:27 -0300

    Refactor footer to make it more flexible

--- src/containers/footer/footer.js
+++ src/containers/footer/footer.js
@@ -1,97 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Redirect } from 'react-router-dom';
 import {
-  Button as MaterialButton,
-  Container,
-  Grid,
-  Typography
+  Container
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-import { useAuth } from 'hooks';
-import { singularOrPlural } from 'utils';
-import { SIZE_PAGE } from 'routes';
+import FooterCommon from './footer-common';
 
-const Footer = ({ forwardButton, location, history }) => {
-  const { user } = useAuth();
-
-  if (!location.state) {
-    return <Redirect to={SIZE_PAGE} />;
-  }
-
-  const { selectedSize, selectedFlavours } = location.state;
-  const { name, slices, flavours } = selectedSize;
-
-  const goBack = e => {
-    e.preventDefault();
-    history.goBack();
-  };
-
+const Footer = ({ children, ...props }) => {
   return (
     <FooterContainer>
       <Container>
-        <Grid container>
-          <OrderContainer>
-            <Typography><b>{user.firstName} o seu pedido é:</b></Typography>
-            <Typography>
-              Pizza <b>{name.toUpperCase()}</b> - {' '}
-              ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
-              {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
-            </Typography>
-            {selectedFlavours && (
-              <Typography>
-                {singularOrPlural(selectedFlavours.length, 'No sabor', 'Nos sabores')}{' '}
-                <b>{selectedFlavours
-                  .map(({ name }) => name)
-                  .join(', ')
-                  .replace(/, (.[^,]*)$/, ' e $1')}
-                </b>
-              </Typography>
-            )}
-          </OrderContainer>
-          <ButtonContainer>
-            <Button onClick={goBack}>Voltar</Button>
-            <Button
-              color='primary'
-              {...forwardButton}
-            />
-          </ButtonContainer>
-        </Grid>
+        {children || <FooterCommon {...props} />}
       </Container>
     </FooterContainer>
   );
 };
 
 Footer.propTypes = {
-  forwardButton: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired
+  children: PropTypes.node
 };
-
-const ButtonContainer = styled(Grid).attrs({
-  item: true
-})`
-  display: flex;
-  align-items: center;
-`;
 
 const FooterContainer = styled.footer`
   box-shadow: 0 0 3px ${({ theme }) => theme.palette.grey[400]};
   padding: ${({ theme }) => theme.spacing(1)}px;
   width: 100%;
-`;
-
-const OrderContainer = styled(Grid).attrs({
-  item: true
-})`
-  flex-grow: 1;
-`;
-
-const Button = styled(MaterialButton).attrs({
-  variant: 'contained'
-})`
-  margin-left: ${({ theme }) => theme.spacing(2)}px;
 `;
 
 export default Footer;
