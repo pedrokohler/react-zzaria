00022a51167e35c1c071f26283e8f96b3d601ddc
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-05-04 21:25:50 -0300

    Refactor main

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
@@ -32,10 +32,87 @@ const Main = () => {
   const { pizza, resetPizza } = usePizza();
   const { addPizzaToOrder, sendOrder } = useOrder();
 
-  const addPizza = () => {
+  const _addPizza = useCallback(() => {
     addPizzaToOrder(pizza);
     resetPizza();
+  }, [addPizzaToOrder, pizza, resetPizza]);
+
+  const _renderQuantityPage = useCallback(({ location }) => {
+    return <ChoosePizzaQuantity location={location} handleButtonClick={_addPizza} />;
+  }, [_addPizza]);
+
+  const _renderFlavoursPageFooter = useCallback(({ location, history }) => (
+    <Footer
+      location={location}
+      history={history}
+      forwardButton={{
+        to: {
+          pathname: QUANTITY_PAGE,
+          state: pizza
+        },
+        disabled: pizza.selectedFlavours.length === 0,
+        component: Link,
+        children: 'Avançar'
+      }}
+    />
+  ), [pizza]);
+
+  const _renderQuantityPageFooter = useCallback(({ location, history }) => (
+    <Footer
+      location={location}
+      history={history}
+      forwardButton={{
+        to: CHECKOUT_PAGE,
+        component: Link,
+        onClick: _addPizza,
+        children: 'Finalizar compra'
+      }}
+    />
+  ), [_addPizza]);
+
+  const _renderCheckoutPageFooter = useCallback(() => (
+    <FooterCheckout>
+      <ButtonLink to={CHECKOUT_CONFIRMATION_PAGE}>
+        <Button variant='contained' color='primary'>Confirmar dados</Button>
+      </ButtonLink>
+    </FooterCheckout>
+  ), []);
+
+  const _renderCheckoutConfirmationFooter = useCallback(() => (
+    <FooterCheckout justifyContent='center'>
+      <ButtonLink to={CHECKOUT_SUCCESS_PAGE}>
+        <Button
+          variant='contained'
+          color='primary'
+          size='large'
+          onClick={sendOrder}
+        >
+          Tudo certo!
+        </Button>
+      </ButtonLink>
+    </FooterCheckout>
+  ), [sendOrder]);
+
+  const _renderCheckoutSuccessFooter = useCallback(() => (
+    <FooterCheckout justifyContent='center'>
+      <ButtonLink to={SIZE_PAGE}>
+        <Button size='large' color='secondary'>
+          {' < '} Voltar para a página inicial
+        </Button>
+      </ButtonLink>
+    </FooterCheckout>
+  ), []);
+
+  const commonPropTypes = useCallback({
+    location: PropTypes.object,
+    history: PropTypes.object
+  }, []);
+
+  _renderQuantityPage.propTypes = {
+    location: PropTypes.object
   };
+  _renderFlavoursPageFooter.propTypes = commonPropTypes;
+  _renderQuantityPageFooter.propTypes = commonPropTypes;
 
   return (
     <>
@@ -49,12 +126,7 @@ const Main = () => {
           <Switch>
             <Route path={SIZE_PAGE} exact component={ChoosePizzaSize} />
             <Route path={FLAVOURS_PAGE} component={ChoosePizzaFlavour} />
-            <Route
-              path={QUANTITY_PAGE}
-              render={({ location }) => {
-                return <ChoosePizzaQuantity location={location} handleButtonClick={addPizza} />;
-              }}
-            />
+            <Route path={QUANTITY_PAGE} render={_renderQuantityPage} />
             <Route exact path={CHECKOUT_PAGE} component={Checkout} />
             <Route path={CHECKOUT_CONFIRMATION_PAGE} component={CheckoutConfirmation} />
             <Route path={CHECKOUT_SUCCESS_PAGE} component={CheckoutSuccess} />
@@ -64,78 +136,11 @@ const Main = () => {
       </Content>
 
       <Switch>
-        <Route
-          path={FLAVOURS_PAGE}
-          render={({ location, history }) => (
-            <Footer
-              location={location}
-              history={history}
-              forwardButton={{
-                to: {
-                  pathname: QUANTITY_PAGE,
-                  state: pizza
-                },
-                disabled: pizza.selectedFlavours.length === 0,
-                component: Link,
-                children: 'Avançar'
-              }}
-            />
-          )}
-        />
-        <Route
-          path={QUANTITY_PAGE}
-          render={({ location, history }) => (
-            <Footer
-              location={location}
-              history={history}
-              forwardButton={{
-                to: CHECKOUT_PAGE,
-                component: Link,
-                onClick: addPizza,
-                children: 'Finalizar compra'
-              }}
-            />
-          )}
-        />
-        <Route
-          exact
-          path={CHECKOUT_PAGE} render={() => (
-            <FooterCheckout>
-              <ButtonLink to={CHECKOUT_CONFIRMATION_PAGE}>
-                <Button variant='contained' color='primary'>Confirmar dados</Button>
-              </ButtonLink>
-            </FooterCheckout>
-          )}
-        />
-        <Route
-          exact
-          path={CHECKOUT_CONFIRMATION_PAGE} render={() => (
-            <FooterCheckout justifyContent='center'>
-              <ButtonLink to={CHECKOUT_SUCCESS_PAGE}>
-                <Button
-                  variant='contained'
-                  color='primary'
-                  size='large'
-                  onClick={sendOrder}
-                >
-                  Tudo certo!
-                </Button>
-              </ButtonLink>
-            </FooterCheckout>
-          )}
-        />
-        <Route
-          exact
-          path={CHECKOUT_SUCCESS_PAGE} render={() => (
-            <FooterCheckout justifyContent='center'>
-              <ButtonLink to={SIZE_PAGE}>
-                <Button size='large' color='secondary'>
-                  {' < '} Voltar para a página inicial
-                </Button>
-              </ButtonLink>
-            </FooterCheckout>
-          )}
-        />
+        <Route path={FLAVOURS_PAGE} render={_renderFlavoursPageFooter} />
+        <Route path={QUANTITY_PAGE} render={_renderQuantityPageFooter} />
+        <Route exact path={CHECKOUT_PAGE} render={_renderCheckoutPageFooter} />
+        <Route exact path={CHECKOUT_CONFIRMATION_PAGE} render={_renderCheckoutConfirmationFooter} />
+        <Route exact path={CHECKOUT_SUCCESS_PAGE} render={_renderCheckoutSuccessFooter} />
       </Switch>
     </>
   );
