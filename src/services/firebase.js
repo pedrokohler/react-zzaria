a504e6de1618cb1672ff744d3e3d13957fe2eb56
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-05-06 22:06:39 -0300

    Change firebase variables to environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,24 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBb0gccAWocgyHl0ifFbz9AnzeEGXhATXo',
-  authDomain: 'react-zzaria-7d8d5.firebaseapp.com',
-  databaseURL: 'https://react-zzaria-7d8d5.firebaseio.com',
-  projectId: 'react-zzaria-7d8d5',
-  storageBucket: 'react-zzaria-7d8d5.appspot.com',
-  messagingSenderId: '554976170245',
-  appId: '1:554976170245:web:5883784a98ff75938a276c'
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
